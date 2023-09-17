156f12788142c939ba46dbdfaa79b7e5aafabf96
Ivan Mendes <ivanmendes2005@gmail.com>
2023-09-17 18:21:41 -0300

    Remocao de arquivos

---
+++ colletions_java_api_list/src/list/ordenacao/OrdenacaoPessoas.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class OrdenacaoPessoas {
+    private List<Pessoa> pessoaList;
+
+    public OrdenacaoPessoas() {
+        this.pessoaList = new ArrayList<>();
+    }
+
+    public void addPessoa(String nome, int idade, double altura) {
+        this.pessoaList.add(new Pessoa(nome, idade, altura));
+    }
+
+    public List<Pessoa> ordenarPorIdade() {
+        List<Pessoa> listPessoasOrdenadoPIdade = new ArrayList<>(pessoaList);
+        Collections.sort(listPessoasOrdenadoPIdade);
+        return listPessoasOrdenadoPIdade;
+    }
+
+    public List<Pessoa> ordenarPorAltura() {
+        List<Pessoa> listPessoasOrdeandoPAltura = new ArrayList<>(pessoaList);
+        Collections.sort(listPessoasOrdeandoPAltura, new ComparatorPorAltura());
+        return listPessoasOrdeandoPAltura;
+    }
+}
